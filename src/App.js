707f1ba3486c719b8fb45c1de5341580e91bb31b
Paloma Lozano <palomalogu9@gmail.com>
2022-01-26 19:22:54 +0100

    make card component reusable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './style/App.css';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
 import Card from './components/card';
+import img1 from './images/image1.png';
+import img2 from './images/wedding.png';
+import img3 from './images/mountain.png';
 
 function App() {
   return (
@@ -11,7 +14,9 @@ function App() {
       </header>
       <main>
         <Hero />
-        <Card />
+        <Card img={img1} />
+        <Card img={img2} />
+        <Card img={img3} />
       </main>
     </div>
   );
--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,21 @@
-import img1 from '../images/image1.png';
-import img2 from '../images/wedding.png';
-import img3 from '../images/mountain.png';
 import starIcon from '../images/star.png';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       <img
-        src={img1}
+        src={props.img}
         alt="happy woman in a swimming tournament"
         className="card--image"
       />
       <div className="card--stats">
         <img src={starIcon} alt="star icon" className="card--star" />
-        <span className="grey">5.0</span>
-        <span className="grey">(6.)</span>
-        <span className="grey">USA</span>
+        <span className="grey">{props.star}</span>
+        <span className="grey">{props.comments}</span>
+        <span className="grey">{props.place}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
-      <p className="bold">From $136/person</p>
+      <p>{props.description}</p>
+      <p className="bold">{props.price}</p>
     </div>
   );
 };
